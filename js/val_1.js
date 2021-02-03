dfc171001338af5c0d1ce929342d2fb68ae16642
franceeesc00 <100006213.joan23@fje.edu>
2021-02-03 11:55:59 +0100

    2

--- js/val_1.js
+++ js/val_1.js
@@ -7,3 +7,73 @@ function val_1() {
         return false
     }
 }
+
+function val_2() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_3() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_4() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_5() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_6() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_7() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
+
+function val_8() {
+    var a = document.getElementById('val_1').value
+    if (a == '13112211') {
+        return true
+    } else {
+        alert('¡Inténtalo de nuevo!')
+        return false
+    }
+}
